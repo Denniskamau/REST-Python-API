a82b27f76c3275a0ea3eeb15bb57eaf395ce09d3
Denniskamau <denniskamau3@gmail.com>
2018-05-01 20:40:27 +0300

    add put and delete endpoint

--- app.py
+++ app.py
@@ -12,19 +12,43 @@ jwt = JWT(app, authenticate, identity)
 items = []
 
 class Item(Resource):
-   
+    #Require an authorization token for it to be executed.
+    @jwt_required()
     def get(self, name):
-        item = filter(lambda x: x['name'] == name, items)
+        #Next returns the first item found by the list function
+        #filter function will only give a filter object hence we use next to return the object
+        item = next(filter(lambda x: x['name'] == name, items), None)
+        #for item in items:
+           #if item['name'] == name:
         return {'item': item}, 200 if item else 404
 
     def post(self, name):
-        if filter(lambda x: x['name'] == name, items):
+        
+        if next(filter(lambda x: x['name'] == name, items), None):
             return{'message': "An item with name '{}' already exists".format(name)}, 400
 
-        data = request.get_json()
+        
         item = {'name': name, 'price':data['price']}
+    
+        data = request.get_json()
+        #item = {'name':name, 'price':'2333'}
         items.append(item)
         return item, 201
+
+    def delete(self,name):
+        global items
+        items = list(filter(lambda x: x['name'] != name, items))
+        return {'message': 'Item deleted'}
+
+    def put(self,name):
+        data = request.get_json()
+        item = next(filter(lambda x: x['name'] == name, items), None)
+        if item is None:
+            item = {'name':name, 'price':data['price']}
+            items.append(item)
+        else:
+            item.update(data)
+        return item
 
 class ItemList(Resource):
     def get(self):
